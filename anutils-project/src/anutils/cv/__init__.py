920c025c136d3a23755110c480d24295624ead11
Anshul Kharbanda <akanshul97@gmail.com>
2025-03-15 20:22:13 -0400

    Update image stitching notebook (stitch multiple images... with some distortions)

--- anutils-project/src/anutils/cv/__init__.py
+++ anutils-project/src/anutils/cv/__init__.py
@@ -4,18 +4,27 @@ import numpy as np
 
 def rect(w, h, tr=(0,0)):
     tr = np.array([*tr])
-    return tr + np.array([
-        [ 0, 0 ],
-        [ w, 0 ],
-        [ w, h ],
-        [ h, 0 ]
-    ])
+    sizes = np.array([[w,h]])
+    return tr + rects(sizes)[0,:,:]
 
 
-def homogenize(p):
+def rects(sizes):
+    rect_indeces = [[0,0],[1,0],[1,2],[0,2]]
+    zeros = np.zeros((sizes.shape[0],1))
+    sizes_exp = np.concatenate((zeros, sizes), axis=1)
+    return sizes_exp[:,rect_indeces]
+    
+
+def homogenize(p, axes=None):
     return np.vstack((
         p.T, np.ones((1,p.shape[0]))
     ))
 
+def homogenize_1d(p):
+    return np.concatenate((p, np.array([1])))
+
 def unhomogenize(h):
     return (h / h[2:,:]).T[:,:2]
+
+def unhomogenize_1d(h):
+    return h[:2] / h[2]
